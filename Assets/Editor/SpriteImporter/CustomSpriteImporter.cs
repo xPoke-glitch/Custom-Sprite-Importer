c67a887c53c024fc13c29d2b4c70955f236627b5
Cristian <cri.ghiorzi@gmail.com>
2022-06-25 15:30:46 +0200

    Custom sprites importer witth preset settings

---
+++ Assets/Editor/SpriteImporter/CustomSpriteImporter.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Presets;
+public class CustomSpriteImporter : AssetPostprocessor
+{ 
+    void OnPreprocessAsset()
+    {
+        TextureImporter texImporter = assetImporter as TextureImporter;
+
+        // Get the preset
+        Preset preset = (Preset) AssetDatabase.LoadAssetAtPath("Assets/Editor/SpriteImporter/Settings/TextureImporterPreset.preset", typeof(Preset));
+
+        if(preset == null)
+        {
+            Debug.LogError("[CustomSpriteImporter OnPreprocessAsset] Settings' preset not found");
+            return;
+        }
+
+        if (texImporter != null)
+        {
+            TextureImporterSettings texSettings = new TextureImporterSettings();
+
+            if (!preset.CanBeAppliedTo(texImporter))
+            {
+                Debug.LogError("Source and target asset types do not match!");
+            }
+            else
+            {
+                // Apply the preset to the importer - custom settings
+                preset.ApplyTo(texImporter);
+            }
+
+            Debug.Log("[CustomSpriteImporter OnPreprocessAsset] Asset Done : " + assetPath);
+        }
+     
+    }
+}
